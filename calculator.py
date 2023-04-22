5ce2dd1fc9de21e836beeed64c220113f6fd6cc1
shubham5223 <shubham5223ssp@gmail.com>
2023-04-22 21:30:58 +0530

    add calculator program

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Calculator Clean code
+first_number = int(input("Enter First Number "))
+second_number = int(input("Enter First Number "))
+
+print("Sum of Two Numbers", first_number + second_number)
+print("Subtraction of Two Numbers", first_number - second_number)
+print("Product of Two Numbers", first_number * second_number)
+print("Division of Two Numbers", first_number / second_number)
